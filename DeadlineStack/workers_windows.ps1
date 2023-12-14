8ede3b828d1af3b7a2c91e9c84de8dca24c37e39
Thierry Delran <tdelran@missioncloud.com>
2023-12-14 17:45:03 +0100

    Removed -OU "computers" when workers joins the ad

--- DeadlineStack/workers_windows.ps1
+++ DeadlineStack/workers_windows.ps1
@@ -4,20 +4,21 @@ $secret=""
 # Fsx drive DNS name
 $drive=""
 
+# Write-Output "forcing deadline connection"
+$DEADLINE_PATH = "C:\Program Files\Thinkbox\Deadline10\bin"
+pushd $DEADLINE_PATH
+.\deadlinecommand.exe -SetIniFileSetting ProxyRoot0 renderqueue.deadline.internal:4433
+.\deadlinecommand.exe -SetIniFileSetting ProxyRoot renderqueue.deadline.internal:4433
+
 Write-Output "Adding computer to domain"
 $adInfo = Get-SECSecretValue -SecretId $secret | Select-Object -ExpandProperty SecretString | ConvertFrom-Json
 $credential = New-Object -TypeName PSCredential -ArgumentList ($adInfo.username + '@' + $adInfo.fqdn),(ConvertTo-SecureString -String $adInfo.password -AsPlainText -Force)[0]
 $username = $adInfo.fqdn+"\"+$adInfo.username
 Add-Computer -DomainName $adInfo.fqdn -Credential $credential -Restart
 
-Write-Output "Mounting fsx drive"
-New-PSDrive -Name "Z" -Root ("\\"+$drive+"\share") -Persist -PSProvider "FileSystem" -Credential $credential
+# Write-Output "Mounting fsx drive"
+# New-PSDrive -Name "Z" -Root ("\\"+$drive+"\share") -Persist -PSProvider "FileSystem" -Credential $credential
 
-Write-Output "forcing deadline connection"
-$DEADLINE_PATH = "C:\Program Files\Thinkbox\Deadline10\bin"
-pushd $DEADLINE_PATH
-.\deadlinecommand.exe -SetIniFileSetting ProxyRoot0 renderqueue.deadline.internal:4433
-.\deadlinecommand.exe -SetIniFileSetting ProxyRoot renderqueue.deadline.internal:4433
 
 
 popd
