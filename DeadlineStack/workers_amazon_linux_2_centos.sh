32f2c81f89098098d2b2db425488de07d12e40db
Thierry Delran <tdelran@missioncloud.com>
2024-02-15 20:13:50 +0100

    updated centos script

--- DeadlineStack/workers_amazon_linux_2_centos.sh
+++ DeadlineStack/workers_amazon_linux_2_centos.sh
@@ -24,7 +24,7 @@ echo $password | sudo realm join -U admin@$capactivedirectory $activedirectory -
 
 echo $password | kinit admin@$capactivedirectory
 sudo mkdir /mnt/studio
-sudo mount -t cifs -o vers=3.0,sec=ntlmsspi,user=Admin@$capactivedirectory,password=$password //$drive/share /mnt/studio
+sudo mount -t cifs -o vers=3.0,sec=ntlmsspi,user=Admin@$capactivedirectory,password=$password,file_mode=0666,dir_mode=0777 //$drive/share /mnt/studio
 
 /opt/Thinkbox/Deadline10/bin/deadlinecommand.exe -SetIniFileSetting ProxyRoot0 "renderqueue.deadline.internal:4433"
 /opt/Thinkbox/Deadline10/bin/deadlinecommand.exe -SetIniFileSetting ProxyRoot "renderqueue.deadline.internal:4433"
