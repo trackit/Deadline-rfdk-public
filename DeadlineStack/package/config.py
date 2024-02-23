b248b4144fe8c1f8b13181d125ca9ec6e578055b
Clarisse Eynard <clarisse.eynard@epitech.eu>
2024-02-23 09:23:34 +0100

    feat(python): Add ECS cluster and task to CDK

--- DeadlineStack/package/config.py
+++ DeadlineStack/package/config.py
@@ -13,17 +13,20 @@ from typing import (
     Mapping,
 )
 
+
 class AppConfig:
     """
     Configuration values for the sample app.
 
     TODO: Fill these in with your own values.
     """
+
     def __init__(self):
         # A map of regions to Deadline Client Linux AMIs. As an example, the base Linux Deadline 10.1.19.4 AMI ID
         # from us-west-2 is filled in. It can be used as-is, added to, or replaced. Ideally the version here
         #  should match the one used for staging the render queue and usage based licensing recipes.
-        self.deadline_client_linux_ami_map: Mapping[str, str] = {'us-west-2': 'ami-067d780e98fe3b09f'}
+        self.deadline_client_linux_ami_map: Mapping[str, str] = {
+            'eu-west-3': 'ami-08afbca41a57b6e72'}
 
         # Whether the DeadlineResourceTrackerAccessRole IAM role required by Deadline's Resource Tracker should be created in this CDK app.
         #
@@ -33,17 +36,17 @@ class AppConfig:
         # Note: Deadline's Resource Tracker only supports being used by a single Deadline Repository per AWS account.
         self.create_resource_tracker_role: bool = True
         # AWS region deadline is deployed into (ex: "us-west-2")
-        self.aws_region:str = ""
+        self.aws_region: str = "eu-west-3"
         # Deadline VPC CIDR required (ex:"172.0.0.0/16")
-        self.vpc_cidr: str = ""
+        self.vpc_cidr: str = "10.2.0.0/16"
         # Bucket for workers script
-        self.s3_bucket_workers: str = ""
+        self.s3_bucket_workers: str = "deadline-workers-scripts-test"
         # S3 bucket worker region (verifiy this on S3 service)
-        self.s3_bucket_workers_region: str = ""
+        self.s3_bucket_workers_region: str = "eu-west-3"
         # EC2 test instance AMI
-        self.custom_ami_id: str = ""
+        self.custom_ami_id: str = "ami-0c75d0e0e7489cfbe"
         # EC2 test instance key pair
-        self.ec2_key_pair_name: str = ""
+        self.ec2_key_pair_name: str = "deadlinetest"
 
         # Spot instance fleet configuration
         # For each fleet, use those parameters:
@@ -56,23 +59,14 @@ class AppConfig:
         # "user_data_script" expecting filename (sh for Linux, ps1 for Windows) is an additional script file you uploaded to the worker S3 bucket
         self.fleet_config: dict = {
             "fleet1": {
-                "name":"Blender",
-                "is_linux":1,
+                "name": "Blender",
+                "is_linux": 1,
                 # "instance_types":[InstanceType.of(InstanceClass.BURSTABLE3, InstanceSize.LARGE)],
-                "instance_types":["m5.large","m5.2xlarge"],
-                "worker_machine_image":"",
-                "max_capacity":1,
-                "allocation_strategy":SpotFleetAllocationStrategy.CAPACITY_OPTIMIZED,
-                "user_data_script":""
-            },
-            "fleet2": {
-                "name":"Maya",
-                "is_linux":1,
-                "instance_types":["m5.large","m5.2xlarge"],
-                "worker_machine_image":"",
-                "max_capacity":1,
-                "allocation_strategy":SpotFleetAllocationStrategy.CAPACITY_OPTIMIZED,
-                "user_data_script":""
+                "instance_types": ["m5.large", "m5.2xlarge"],
+                "worker_machine_image": "ami-08afbca41a57b6e72",
+                "max_capacity": 1,
+                "allocation_strategy": SpotFleetAllocationStrategy.CAPACITY_OPTIMIZED,
+                "user_data_script": ""
             }
         }
 
--- DeadlineStack/package/lib/deadline_stack.py
+++ DeadlineStack/package/lib/deadline_stack.py
@@ -1,6 +1,16 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+from os import path
+from aws_cdk import aws_ecs as ecs
+from aws_cdk.aws_ecr import Repository as EcrRepository
+from aws_cdk.aws_ecr_assets import DockerImageAsset
+# from aws_cdk.aws_ecr_deployment import (
+#     DockerImageName,
+#     Source,
+#     TagParameter,
+# )
+
 from dataclasses import dataclass
 from aws_cdk import (
     Duration,
@@ -100,17 +110,17 @@ class DeadlineStackProps(StackProps):
     ec2_key_pair_name: str
 
 
-
 # USER DATA Handling
 class UserDataProvider(InstanceUserDataProvider):
     def __init__(self, scope: Construct, stack_id: str, *, props: DeadlineStackProps, os_key: int, user_data_script=None, **kwargs):
         super().__init__(scope, stack_id, **kwargs)
-        self.props=props
-        self.os_key=os_key
-        self.user_data_script=user_data_script
+        self.props = props
+        self.os_key = os_key
+        self.user_data_script = user_data_script
 
     def pre_render_queue_configuration(self, host) -> None:
-        host.user_data.add_commands("echo 'Entering preRenderQueueConfiguration'")
+        host.user_data.add_commands(
+            "echo 'Entering preRenderQueueConfiguration'")
 
         try:
             license_bucket = Bucket.from_bucket_attributes(
@@ -126,26 +136,37 @@ class UserDataProvider(InstanceUserDataProvider):
                     bucket_key=f'deadline/{self.user_data_script}',
                     region=self.props.s3_bucket_workers_region
                 )
-                host.user_data.add_commands(f"echo 'Downloaded user data script to {user_data_path}'")
-                host.user_data.add_execute_file_command(file_path=user_data_path)
+                host.user_data.add_commands(
+                    f"echo 'Downloaded user data script to {user_data_path}'")
+                host.user_data.add_execute_file_command(
+                    file_path=user_data_path)
                 host.user_data.add_commands("echo 'Executed user data script'")
             else:
-                host.user_data.add_commands("echo 'No user_data_script provided'")
+                host.user_data.add_commands(
+                    "echo 'No user_data_script provided'")
 
         except Exception as e:
             host.user_data.add_commands(f"echo 'Error: {str(e)}'")
 
-        host.user_data.add_commands("echo 'Exiting preRenderQueueConfiguration'")
+        host.user_data.add_commands(
+            "echo 'Exiting preRenderQueueConfiguration'")
+
     def pre_worker_configuration(self, host) -> None:
         if self.os_key == 1:
-            host.user_data.add_commands("/opt/Thinkbox/Deadline10/bin/deadlinecommand -SetIniFileSetting ProxyRoot0 'renderqueue.deadline.internal:4433'")
-            host.user_data.add_commands("/opt/Thinkbox/Deadline10/bin/deadlinecommand -SetIniFileSetting ProxyRoot 'renderqueue.deadline.internal:4433'")
+            host.user_data.add_commands(
+                "/opt/Thinkbox/Deadline10/bin/deadlinecommand -SetIniFileSetting ProxyRoot0 'renderqueue.deadline.internal:4433'")
+            host.user_data.add_commands(
+                "/opt/Thinkbox/Deadline10/bin/deadlinecommand -SetIniFileSetting ProxyRoot 'renderqueue.deadline.internal:4433'")
         else:
-            host.user_data.add_commands(r"$DEADLINE_PATH = 'C:\Program Files\Thinkbox\Deadline10\bin'")
+            host.user_data.add_commands(
+                r"$DEADLINE_PATH = 'C:\Program Files\Thinkbox\Deadline10\bin'")
             host.user_data.add_commands("pushd $DEADLINE_PATH")
-            host.user_data.add_commands(".\deadlinecommand.exe -SetIniFileSetting ProxyRoot0 'renderqueue.deadline.internal:4433'")
-            host.user_data.add_commands(".\deadlinecommand.exe -SetIniFileSetting ProxyRoot 'renderqueue.deadline.internal:4433'")
+            host.user_data.add_commands(
+                ".\deadlinecommand.exe -SetIniFileSetting ProxyRoot0 'renderqueue.deadline.internal:4433'")
+            host.user_data.add_commands(
+                ".\deadlinecommand.exe -SetIniFileSetting ProxyRoot 'renderqueue.deadline.internal:4433'")
             pass
+
 
 class DeadlineStack(Stack):
     """
@@ -162,63 +183,110 @@ class DeadlineStack(Stack):
         """
         super().__init__(scope, stack_id, **kwargs)
 
-
         # Create Cloud9 IAM group
-        cloud9IamGroup= Group(self, "Cloud9Admin")
-        cloud9IamGroup.add_managed_policy(ManagedPolicy.from_aws_managed_policy_name('AWSCloud9Administrator'))
+        cloud9IamGroup = Group(self, "Cloud9Admin")
+        cloud9IamGroup.add_managed_policy(
+            ManagedPolicy.from_aws_managed_policy_name('AWSCloud9Administrator'))
 
-
-         # The VPC that all components of the render farm will be created in.
+        # The VPC that all components of the render farm will be created in.
         vpc = Vpc(
             self,
             'Vpc',
             max_azs=99,
             nat_gateways=1,
-            cidr = props.vpc_cidr,
-            subnet_configuration = [SubnetConfiguration(subnet_type=SubnetType.PUBLIC,
+            cidr=props.vpc_cidr,
+            subnet_configuration=[SubnetConfiguration(subnet_type=SubnetType.PUBLIC,
                                                       cidr_mask=28,
                                                       name="public"),
-                                    SubnetConfiguration(subnet_type=SubnetType.PRIVATE_WITH_EGRESS,
+                                  SubnetConfiguration(subnet_type=SubnetType.PRIVATE_WITH_EGRESS,
                                                       cidr_mask=19,
                                                       name="render-")
-                                    ]
+                                  ]
         )
+        
+
+        # Create an ECR repository
+        ecr_repository = EcrRepository(
+            self,
+            'Spotfleet-Mgmt-UI-ECR-Repository',
+            image_scan_on_push=True,  # Enable image scanning on push
+            removal_policy=RemovalPolicy.DESTROY  # Adjust the removal policy as needed
+        )
+
+        # Create an asset from the spotfleet-mgmt-ui directory
+        asset = DockerImageAsset(
+            self,
+            "sfmt",
+            directory="./spotfleet-mgmt-ui",
+        )
+
+        # Deploy the asset to the ECR repository
+
+        # ecr_repository.grant_pull_push(asset.grant_principal)
+        # asset.repository = ecr_repository
+
+        # ecr_repository.on_image_scan_completed(
+        #     "ImageScanComplete").add_target(asset)
+
+        # docker_image_asset_hash = asset.asset_hash
+        # destination_image_name = f"{ecr_repository.repository_uri}:{docker_image_asset_hash}"
+
+        # ecr_deploy = ecr_deployment.ECRDeployment(
+        #     self,
+        #     "EcrDeployment",
+        #     src=ecr_deployment.DockerImageName(
+        #         self.docker_image_asset.image_uri),
+        #     dest=ecr_deployment.DockerImageName(destination_image_name),
+        # )
+
+        # Create an ECS cluster
+        ecs_cluster = ecs.Cluster(
+            self,
+            'Spotfleet-Mgmt-UI-ECS-Cluster',
+            vpc=vpc
+        )
+
+        # Create an ECS task definition
+        # This task must run the image present in the ECR repo
+
 
         # security group for resolver endpoint
         worker_resolver_endpoint_sg = SecurityGroup(
-                self,
-                "Deadline_workers_to_ad_sg",
-                vpc=vpc,
-                allow_all_outbound=True,
-                description="Deadline_workers_to_ad",
-                security_group_name="deadline_worker_to_ad"
+            self,
+            "Deadline_workers_to_ad_sg",
+            vpc=vpc,
+            allow_all_outbound=True,
+            description="Deadline_workers_to_ad",
+            security_group_name="deadline_worker_to_ad"
 
-            )
+        )
         worker_resolver_endpoint_sg.add_ingress_rule(
-                peer=Peer.ipv4(props.vpc_cidr),
-                connection=Port.tcp(53),
-                description="allow workers to connect to studio active directory"
-            )
+            peer=Peer.ipv4(props.vpc_cidr),
+            connection=Port.tcp(53),
+            description="allow workers to connect to studio active directory"
+        )
 
         # create worker IAM role
 
         fleet_instance_role = Role(
-                self,
-                'FleetRole',
-                role_name= 'Deadline-Fleet-Instance-Role',
-                assumed_by=ServicePrincipal('ec2.amazonaws.com'),
-                managed_policies= [ManagedPolicy.from_aws_managed_policy_name('AWSThinkboxDeadlineSpotEventPluginWorkerPolicy')],
-                inline_policies= {
-                    "s3_licence_access": PolicyDocument(
-                        statements=[
-                            PolicyStatement(
-                                actions=["s3:GetObject"],
-                                resources=["arn:aws:s3:::"+props.s3_bucket_workers+"/*"]
-                            )
-                        ]
-                    )
-                },
-            )
+            self,
+            'FleetRole',
+            role_name='Deadline-Fleet-Instance-Role',
+            assumed_by=ServicePrincipal('ec2.amazonaws.com'),
+            managed_policies=[ManagedPolicy.from_aws_managed_policy_name(
+                'AWSThinkboxDeadlineSpotEventPluginWorkerPolicy')],
+            inline_policies={
+                "s3_licence_access": PolicyDocument(
+                    statements=[
+                        PolicyStatement(
+                            actions=["s3:GetObject"],
+                            resources=["arn:aws:s3:::" +
+                                       props.s3_bucket_workers+"/*"]
+                        )
+                    ]
+                )
+            },
+        )
 
         recipes = ThinkboxDockerRecipes(
             self,
@@ -294,7 +362,8 @@ class DeadlineStack(Stack):
                 internal_protocol=ApplicationProtocol.HTTPS,
             ),
         )
-         render_queue.connections.allow_default_port_from(Peer.ipv4(props.vpc_cidr))
+        render_queue.connections.allow_default_port_from(
+            Peer.ipv4(props.vpc_cidr))
 
         if props.create_resource_tracker_role:
             # Creates the Resource Tracker Access role. This role is required to exist in your account so the resource tracker will work properly
@@ -302,58 +371,62 @@ class DeadlineStack(Stack):
                 self,
                 'ResourceTrackerRole',
                 assumed_by=ServicePrincipal('lambda.amazonaws.com'),
-                managed_policies= [ManagedPolicy.from_aws_managed_policy_name('AWSThinkboxDeadlineResourceTrackerAccessPolicy')],
-                role_name= 'DeadlineResourceTrackerAccessRole',
+                managed_policies=[ManagedPolicy.from_aws_managed_policy_name(
+                    'AWSThinkboxDeadlineResourceTrackerAccessPolicy')],
+                role_name='DeadlineResourceTrackerAccessRole',
             )
 
         # Spot fleet Security group
         worker_fleet_sg = SecurityGroup(
-                self,
-                "Deadline_workers_fleet",
-                vpc=vpc,
-                allow_all_outbound=True,
-                description="Deadline_workers_fleet",
-                security_group_name="deadline_workers_fleet"
+            self,
+            "Deadline_workers_fleet",
+            vpc=vpc,
+            allow_all_outbound=True,
+            description="Deadline_workers_fleet",
+            security_group_name="deadline_workers_fleet"
 
-            )
+        )
 
         # iterate through props.fleet_config dict
-        spot_fleet=[];
+        spot_fleet = []
         for i, fleet in props.fleet_config.items():
             # determine worker AMI OS
             if fleet["is_linux"] == 1:
-                ami=MachineImage.generic_linux({props.aws_region:fleet["worker_machine_image"]})
+                ami = MachineImage.generic_linux(
+                    {props.aws_region: fleet["worker_machine_image"]})
             else:
-                ami=MachineImage.generic_windows({props.aws_region:fleet["worker_machine_image"]})
+                ami = MachineImage.generic_windows(
+                    {props.aws_region: fleet["worker_machine_image"]})
             # Format workstation list
-            instance_type_format_list= []
+            instance_type_format_list = []
             for l in fleet["instance_types"]:
-                instance_type_format= InstanceType(l)
+                instance_type_format = InstanceType(l)
                 instance_type_format_list.append(instance_type_format)
             if "user_data_script" in fleet:
-                user_data_script=fleet["user_data_script"]
+                user_data_script = fleet["user_data_script"]
             else:
-                user_data_script=None
+                user_data_script = None
             spot_fleet_config = SpotEventPluginFleet(
-                        self,
-                        f'{fleet["name"]}_spot_event_plugin_fleet',
-                        vpc=vpc,
-                        render_queue=render_queue,
-                        deadline_groups=[f'{fleet["name"]}_group'],
-                        security_groups=[worker_fleet_sg],
-                        instance_types=instance_type_format_list,
-                        worker_machine_image=ami,
-                        max_capacity=fleet["max_capacity"],
-                        fleet_instance_role=fleet_instance_role,
-                        # use the following parameter this if you need to connect to workers
-                        # key_name=key_pair_name,
-                        allocation_strategy=fleet["allocation_strategy"],
-                        user_data_provider=UserDataProvider(
-                            self, f'{fleet["name"]}_user_data_provider', props=props, os_key=fleet["is_linux"], user_data_script=user_data_script),
+                self,
+                f'{fleet["name"]}_spot_event_plugin_fleet',
+                vpc=vpc,
+                render_queue=render_queue,
+                deadline_groups=[f'{fleet["name"]}_group'],
+                security_groups=[worker_fleet_sg],
+                instance_types=instance_type_format_list,
+                worker_machine_image=ami,
+                max_capacity=fleet["max_capacity"],
+                fleet_instance_role=fleet_instance_role,
+                # use the following parameter this if you need to connect to workers
+                # key_name=key_pair_name,
+                allocation_strategy=fleet["allocation_strategy"],
+                user_data_provider=UserDataProvider(
+                    self, f'{fleet["name"]}_user_data_provider', props=props, os_key=fleet["is_linux"], user_data_script=user_data_script),
             )
 
             # Optional: Add additional tags to both spot fleet request and spot instances.
-            Tags.of(spot_fleet_config).add('fleet', f'Deadline-{fleet["name"]}')
+            Tags.of(spot_fleet_config).add(
+                'fleet', f'Deadline-{fleet["name"]}')
             spot_fleet.append(spot_fleet_config)
 
         ConfigureSpotEventPlugin(
@@ -383,18 +456,18 @@ class DeadlineStack(Stack):
             description="Allow RDP access from any source"
         )
 
-
         # IAM Role for EC2 Fleet Connect through AWS Systems Manager
         ssm_role = Role(
             self,
             "SSMInstanceRole",
             assumed_by=ServicePrincipal("ec2.amazonaws.com"),
             managed_policies=[
-                ManagedPolicy.from_aws_managed_policy_name("AmazonSSMManagedInstanceCore"),
-                ManagedPolicy.from_aws_managed_policy_name("service-role/AmazonEC2RoleforSSM")
+                ManagedPolicy.from_aws_managed_policy_name(
+                    "AmazonSSMManagedInstanceCore"),
+                ManagedPolicy.from_aws_managed_policy_name(
+                    "service-role/AmazonEC2RoleforSSM")
             ]
         )
-
 
         # IAM policy to allow EC2 instance to access the S3 bucket
         s3_access_policy = PolicyStatement(
@@ -409,15 +482,18 @@ class DeadlineStack(Stack):
         instance = Instance(
             self,
             "RepositoryAccessInstance",
-            instance_type=InstanceType.of(InstanceClass.COMPUTE5, InstanceSize.LARGE),  # c5.large
-            machine_image=MachineImage.generic_windows({props.aws_region: props.custom_ami_id}),  # Custom Windows AMI with Deadline client
+            instance_type=InstanceType.of(
+                InstanceClass.COMPUTE5, InstanceSize.LARGE),  # c5.large
+            # Custom Windows AMI with Deadline client
+            machine_image=MachineImage.generic_windows(
+                {props.aws_region: props.custom_ami_id}),
             vpc=vpc,
-            vpc_subnets={"subnet_type": SubnetType.PUBLIC},  # Deploy in a public subnet
+            # Deploy in a public subnet
+            vpc_subnets={"subnet_type": SubnetType.PUBLIC},
             security_group=ssm_sg,
             role=ssm_role,
             key_name=props.ec2_key_pair_name
         )
-
 
         # Adding user data to connect to the repository
         instance.user_data.add_commands(
@@ -425,11 +501,11 @@ class DeadlineStack(Stack):
             # Add commands here to configure the instance to connect to the repository
         )
 
-
         # Modify user data to download the certificate from S3
         instance.user_data.add_commands(
             "New-Item -ItemType Directory -Path C:\\deadline -Force",
-            "Read-S3Object -BucketName " + props.s3_bucket_workers + " -Key 'deadline/ca.crt' -File 'C:\\deadline\\ca.crt' -ErrorAction Stop",
+            "Read-S3Object -BucketName " + props.s3_bucket_workers +
+            " -Key 'deadline/ca.crt' -File 'C:\\deadline\\ca.crt' -ErrorAction Stop",
             "Write-Output 'configuring deadline connection'",
             "$DEADLINE_PATH = 'C:\\Program Files\\Thinkbox\\Deadline10\\bin'",
             "pushd $DEADLINE_PATH",
@@ -440,4 +516,3 @@ class DeadlineStack(Stack):
             ".\\deadlinecommand.exe -SetIniFileSetting ClientSSLAuthentication NotRequired",
             ".\\deadlinecommand.exe -SetIniFileSetting ProxyRoot renderqueue.deadline.internal:4433"
         )
-
