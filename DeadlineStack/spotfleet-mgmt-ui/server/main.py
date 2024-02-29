53ed20cb8750ce7a4ef168f4f647d4428da5a0dc
maram <maram@trackit.io>
2024-02-29 12:04:27 +0100

    create healthcheck api

--- DeadlineStack/spotfleet-mgmt-ui/server/main.py
+++ DeadlineStack/spotfleet-mgmt-ui/server/main.py
@@ -1,13 +1,8 @@
 from fastapi import FastAPI
-from fastapi.responses import FileResponse, HTMLResponse
+from fastapi.responses import FileResponse, HTMLResponse, JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi_versioning import VersionedFastAPI
 
 app = FastAPI()
-
-app = VersionedFastAPI(app,
-    version_format='{major}',
-    prefix_format='/v{major}')
 
 app.add_middleware(
     CORSMiddleware,
@@ -21,3 +16,7 @@ app.add_middleware(
 @app.get("/", response_class=HTMLResponse)
 async def serve_client():
     return FileResponse("../client/build/index.html")
+
+@app.get("/healthcheck")
+async def health_check():
+    return JSONResponse(status_code=200, content={"status": "OK"})
