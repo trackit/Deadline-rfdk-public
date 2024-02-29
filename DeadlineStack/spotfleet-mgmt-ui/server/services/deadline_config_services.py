9ce275ea2f083907e01d96831ecc0534aa2f76d1
Clarisse Eynard <clarisse.eynard@epitech.eu>
2024-02-29 17:32:59 +0100

    feat(api): create Deadline config service structure

---
+++ DeadlineStack/spotfleet-mgmt-ui/server/services/deadline_config_services.py
@@ -0,0 +1,9 @@
+
+class DeadlineConfigService:
+    def create_backup(self):
+        print("Creating backup")
+        return
+
+    def update_config(self):
+        print("Updating config")
+        return
