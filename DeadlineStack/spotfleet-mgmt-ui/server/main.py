8cabadd8ac479e7981c02b45eb985dfee7977946
maram <maram@trackit.io>
2024-03-01 14:52:24 +0100

    add api suffix to all api routes

--- DeadlineStack/spotfleet-mgmt-ui/server/main.py
+++ DeadlineStack/spotfleet-mgmt-ui/server/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, APIRouter
 from fastapi.responses import FileResponse, HTMLResponse, JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-
 from routes.fleets import router as fleets_router
 
 app = FastAPI()
@@ -14,25 +13,21 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-app.include_router(fleets_router)
-
-router = APIRouter()
+router = APIRouter(
+    prefix="/api/v1",
+)
 
 @router.get("/", response_class=HTMLResponse)
 async def serve_client():
     return FileResponse("build/index.html")
 
-app.include_router(
-    router,
-    prefix="/api/v1",
-    tags=["api/v1"],
-    dependencies=[],
-    responses={},
-)
-
-
-@app.get("/healthcheck")
+@router.get("/healthcheck")
 async def health_check():
     return JSONResponse(status_code=200, content={"status": "OK"})
 
+app.include_router(router)
+
+app.include_router(
+    fleets_router, 
+    prefix="/api/v1",
+)
