904088edbe7a99da226503c63234f2afbf3f0185
Clarisse Eynard <clarisse.eynard@epitech.eu>
2024-03-05 16:58:11 +0100

    feat(api): implement post/fleets route

--- DeadlineStack/spotfleet-mgmt-ui/server/routes/fleets.py
+++ DeadlineStack/spotfleet-mgmt-ui/server/routes/fleets.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from services.backup import Backup
+from services.schema_validator import SchemaValidator, SchemaValidationException
 
 router = APIRouter(
     prefix="/fleets",
@@ -18,8 +20,22 @@ async def get_fleet(fleet_id: str):
 
 
 @router.post("/", status_code=200)
-async def create_fleet():
-    return {"body": "new"}
+async def create_fleet(deadline_config_path: str, output_backup_path: str, config_schema_path: str = "schemas/fleet_config.json"):
+    try:
+        data = SchemaValidator().validate(deadline_config_path, config_schema_path)
+        backup_result = Backup().create(deadline_config_path, output_backup_path)
+
+        if backup_result:
+            return {"status": "ok", "backup_path": backup_result, "fleet_data": data}
+        else:
+            raise HTTPException(
+                status_code=500, detail="Backup creation failed.")
+    except SchemaValidationException as e:
+        raise HTTPException(
+            status_code=422, detail=str(e))
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"Error processing the request: {str(e)}")
 
 
 @router.put("/{fleet_id}", status_code=200)
---
+++ DeadlineStack/spotfleet-mgmt-ui/server/routes/tests/test_post_fleets.py
@@ -0,0 +1,68 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parents[2]))
+from fastapi.testclient import TestClient
+from ...main import app, API_PREFIX
+
+client = TestClient(app)
+
+deadline_config_path = "server/schemas/config_exemple.json"
+deadline_invalid_config_path = "server/schemas/invalid_config_exemple.json"
+output_backup_path = "server/schemas/backup.json"
+fleet_config_path = "server/schemas/fleet_config.json"
+
+
+def test_post_fleets_valid_data():
+    response = client.post(
+        f"{API_PREFIX}/fleets",
+        params={
+            "deadline_config_path": deadline_config_path,
+            "output_backup_path": output_backup_path,
+            "config_schema_path": fleet_config_path,
+        },
+    )
+    assert response.status_code == 200
+    assert "status" in response.json()
+    assert response.json()["status"] == "ok"
+
+
+def test_post_fleets_invalid_schema():
+    response = client.post(
+        f"{API_PREFIX}/fleets",
+        params={
+            "deadline_config_path": deadline_invalid_config_path,
+            "output_backup_path": output_backup_path,
+            "config_schema_path": fleet_config_path,
+        },
+    )
+    assert response.status_code == 422
+    assert "detail" in response.json()
+    assert "Validation error" in response.json()["detail"]
+
+
+def test_post_fleets_invalid_input():
+    response = client.post(
+        f"{API_PREFIX}/fleets",
+        params={
+            "deadline_config_path": "invalid_path",
+            "output_backup_path": output_backup_path,
+            "config_schema_path": fleet_config_path,
+        },
+    )
+    assert response.status_code == 422
+    assert "detail" in response.json()
+    assert "File not found" in response.json()["detail"]
+
+
+def test_post_fleets_invalid_output():
+    response = client.post(
+        f"{API_PREFIX}/fleets",
+        params={
+            "deadline_config_path": deadline_config_path,
+            "output_backup_path": deadline_config_path,
+            "config_schema_path": fleet_config_path,
+        },
+    )
+    assert response.status_code == 500
+    assert "detail" in response.json()
+    assert "Backup creation failed" in response.json()["detail"]
