e850ce0e5e75f551b861b8f1d725a29bbdb1d13b
Clarisse Eynard <clarisse.eynard@epitech.eu>
2024-03-13 11:34:22 +0100

    feat(frontend): remove useless switch

--- DeadlineStack/spotfleet-mgmt-ui/client/src/components/Structure.tsx
+++ DeadlineStack/spotfleet-mgmt-ui/client/src/components/Structure.tsx
@@ -1,54 +1,28 @@
 import React, { useState } from 'react';
-import { Typography, Switch, Row, Col } from 'antd';
+import { Typography, Row, Col } from 'antd';
+import JsonPreviewCard from './JsonPreviewCard';
 
-const { Title, Text } = Typography;
+const { Title } = Typography;
 
 interface StructureProps {
     data: Record<string, any>;
 }
 
 const Structure: React.FC<StructureProps> = ({ data }) => {
-    const [isSwitchOn, setIsSwitchOn] = useState(true);
     const [jsonData, setData] = useState(data);
 
-    const onChange = (checked: boolean) => {
-        setIsSwitchOn(checked);
-    };
-
-    // use to update json preview
     const updateData = (updatedData: Record<string, any>) => {
         setData(updatedData);
     };
-
-    const renderDeadlineInfo = () => {
-        if (isSwitchOn) {
-            return (
-                <div style={{ marginBottom: '16px' }}>
-                    {/* Add Deadline info here */}
-                </div>
-            );
-        }
-        return null;
-    };
-
     return (
         <div style={{ padding: '16px' }}>
-            <Row justify="space-between" align="middle" style={{ marginBottom: '16px', alignItems: 'flex-start' }}>
-                <Title level={3}>SFMT</Title>
-                <Row>
-                    <Text type="secondary" style={{ marginRight: '8px' }}>General infos</Text>
-                    <Switch defaultChecked={isSwitchOn} onChange={onChange} />
-                </Row>
-            </Row>
+            <Title level={3}>SFMT</Title>
             <Row gutter={16}>
                 <Col span={10}>
                     {/* Add fleet configuration here */}
                 </Col>
                 <Col span={14}>
-                    {renderDeadlineInfo()}
-                    <div>
-                        {/* Add JsonPreview here */}
-                    </div>
+                    <JsonPreviewCard data={jsonData} onDataUpdate={updateData}/>
                 </Col>
             </Row>
         </div>
