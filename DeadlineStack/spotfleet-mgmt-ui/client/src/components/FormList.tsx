1f3f9754aaf4f82fd8affe06ee29c7f3fdd304e2
maram <maram@trackit.io>
2024-03-21 11:47:06 +0100

    feat(frontend): change overrides display

--- DeadlineStack/spotfleet-mgmt-ui/client/src/components/FormList.tsx
+++ DeadlineStack/spotfleet-mgmt-ui/client/src/components/FormList.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Button, Form, Input, Space, Typography } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
+import CustomTagInput from "./CustomTagInput";
 
 interface FormListProps {
-  name: (string | number)[];
-  subItems: string[]; // Array of subitem names
+  name: any[];
+  subItems: any[]; 
 }
 
-const FormList: React.FC<FormListProps> = ({ name, subItems }) => (
-  <>
+const FormList: React.FC<FormListProps> = ({ name, subItems }) => {
+    console.log(name)
+ return (
+    <>
     <Typography.Title level={5}>{name[name.length - 1]}</Typography.Title>
     <Form.List name={name}>
       {(fields, { add, remove }) => (
@@ -21,7 +24,11 @@ const FormList: React.FC<FormListProps> = ({ name, subItems }) => (
                   key={`${name}-${subItem}`}
                   name={[name, subItem]}
                 >
-                  <Input placeholder={subItem} />
+                  {subItem === 'InstanceType' ? (
+                    <Input placeholder={subItem} />
+                  ) : (
+                    <Input />
+                  )}
                 </Form.Item>
               ))}
               <Button onClick={() => remove(name)}>Remove</Button>
@@ -29,13 +36,14 @@ const FormList: React.FC<FormListProps> = ({ name, subItems }) => (
           ))}
           <Form.Item>
             <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
-              Add Override
+              Add 
             </Button>
           </Form.Item>
         </>
       )}
     </Form.List>
   </>
-);
+ );
+};
 
 export default FormList;
