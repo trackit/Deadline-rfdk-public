f5e908085ae5bd32742d6660830ee81a3caae2eb
maram <maram@trackit.io>
2024-03-25 09:14:57 +0100

    feat(frontend): add onChange function for priority

--- DeadlineStack/spotfleet-mgmt-ui/client/src/components/DropDownSelector.tsx
+++ DeadlineStack/spotfleet-mgmt-ui/client/src/components/DropDownSelector.tsx
@@ -5,14 +5,16 @@ interface GeneralInfosProps {
     label: string;
     name: string[];
     items: string[];
+    onChange?: (value: string) => void;
 }
 
-const DropDownSelector: React.FC<GeneralInfosProps> = ({ label, name, items }) => {
+const DropDownSelector: React.FC<GeneralInfosProps> = ({ label, name, items, onChange }) => {
     return (
         <Form.Item label={label} name={name}>
             <Select
                 style={{ width: "40%" }}
                 options={items.map((item) => ({ label: item, value: item }))}
+                onChange={onChange}
             />
         </Form.Item >
     );
