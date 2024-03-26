e6fd6213c2917ba3c5d09453cdaf6533ecfd8974
maram <maram@trackit.io>
2024-03-26 12:06:37 +0100

    feat(front-end): make form adapted to json card preview

--- DeadlineStack/spotfleet-mgmt-ui/client/src/data/config.ts
+++ DeadlineStack/spotfleet-mgmt-ui/client/src/data/config.ts
@@ -1,69 +1,66 @@
 
 const fleetData = {
-    "Fleet 2":{
-        AllocationStrategy:"capacityOptimized",
-        IamFleetRole:"arn:aws:iam::[AWS-ACCOUNT]:role/aws-ec2-spot-fleet-tagging-role",
-        LaunchSpecifications:[],
-        LaunchTemplateConfigs:[
-            {
-                "LaunchTemplateSpecification": {
-                   "LaunchTemplateId": "lt-0763bb2b7c53ddf05",
-                   "Version": "$Latest"
-                },
-                "Overrides": [
-                   {
-                      "InstanceType": "c5.xlarge",
-                      "Priority": 2,
-                      "SubnetId": "subnet-1"
-                   }
-                ]
+   "fleet_1": {
+       "AllocationStrategy": "capacityOptimized",
+       "IamFleetRole": "arn:aws:iam::576872909007:role/aws-ec2-spot-fleet-tagging-role",
+       "LaunchSpecifications": [],
+       "LaunchTemplateConfigs": [
+           {
+               "LaunchTemplateSpecification": {
+                 "LaunchTemplateId": "lt-0b2262ba6c677f0a0",
+                 "Version": "$Latest"
+               },
+               "Overrides": [
+                 {
+                   "InstanceType": "c5.large",
+                   "SubnetId": "subnet-00d3a467fbbc0c549"
+                 }
+               ]
              },
              {
-                "LaunchTemplateSpecification": {
-                   "LaunchTemplateId": "lt-0763bb2b7c53ddf05",
-                   "Version": "$Latest"
-                },
-                "Overrides": [
-                   {
-                      "InstanceType": "c5.xlarge",
-                      "Priority": 2,
-                      "SubnetId": "subnet-2"
-                   }
-                ]
+               "LaunchTemplateSpecification": {
+                 "LaunchTemplateId": "lt-0b2262ba6c677f0a0",
+                 "Version": "$Latest"
+               },
+               "Overrides": [
+                 {
+                   "InstanceType": "c5.large",
+                   "SubnetId": "subnet-0c0d96cc0ca51d9ca"
+                 }
+               ]
              },
-            {
-                "LaunchTemplateSpecification": {
-                   "LaunchTemplateId": "lt-0763bb2b7c53ddf05",
-                   "Version": "$Latest"
-                },
-                "Overrides": [
+             {
+               "LaunchTemplateSpecification": {
+                 "LaunchTemplateId": "lt-0b2262ba6c677f0a0",
+                 "Version": "$Latest"
+               },
+               "Overrides": [
+                 {
+                   "InstanceType": "c5.large",
+                   "SubnetId": "subnet-0cff6ec4642a1a9b1"
+                 }
+               ]
+             }
+       ],
+       "ReplaceUnhealthyInstances": true,
+       "TargetCapacity": 1,
+       "TerminateInstancesWithExpiration": true,
+       "Type": "maintain",
+       "TagSpecifications": [
+           {
+               "ResourceType": "spot-fleet-request",
+               "Tags": [
                    {
-                      "InstanceType": "t3.small",
-                      "Priority": 2,
-                      "SubnetId": "subnet-1"
+                       "Value": "0.42.0:SpotEventPluginFleet",
+                       "Key": "aws-rfdk"
+                   },
+                   {
+                       "Value": "1.1.9",
+                       "Key": "deployedByStudioBuilderVersion"
                    }
-                ]
-             },
-      ],
-      ReplaceUnhealthyInstances:true,
-      TargetCapacity:2,
-      TerminateInstancesWithExpiration:true,
-      Type:"maintain",
-      TagSpecifications:[
-        {
-            ResourceType:"spot-fleet-request",
-            Tags:[
-                {
-                    Value:"[PROJECT-VALUE]",
-                    Key:"project"
-                }
-
-           ]
-
-        }
-
-     ]
-
+               ]
+           }
+       ]
    }
 };
 
